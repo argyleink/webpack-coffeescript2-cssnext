291524356871360860327c5d59d0cab8b914efac
Adam Argyle <atom@argyleink.com>
2017-11-08 20:50:07 -0800

    cleanup, hmr styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,49 @@
 const webpack             = require('webpack')
 const path                = require('path')
 const HtmlWebpackPlugin   = require('html-webpack-plugin')
-const ExtractTextPlugin   = require('extract-text-webpack-plugin')
+// const ExtractTextPlugin   = require('extract-text-webpack-plugin')
 const CleanWebpackPlugin  = require('clean-webpack-plugin')
+const postcssImport       = require('postcss-import')
 const Dashboard           = require('webpack-dashboard/plugin')
 
 module.exports = {
-  context: path.resolve(__dirname, 'app'),
-  entry: {
-    app: './js/app.coffee'
-  },
-  module: {
-    rules: [
-      // coffeescript
-      {
-        test: /\.coffee$/,
-        use: [
-          {
-            loader: 'coffee-loader',
-            options: { sourceMap: true }
-          }
-        ]
-      },
-      // cssnext
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: { importLoaders: 1 },
-            },
-            'postcss-loader',
-          ],
-        })
-      }
-    ]
-  },
+  context:  path.resolve(__dirname, 'app'),
+  entry:    { app: './js/app.coffee' },
+  module: {rules:[
+    {
+      test: /\.coffee$/,
+      use: [{
+        loader: 'coffee-loader',
+        options: { sourceMap: true }
+      }]
+    },
+    {
+      test: /\.css$/,
+      // use: ExtractTextPlugin.extract({
+      //   use: [
+      //     'style-loader',
+      //     { loader: 'css-loader', options: { importLoaders: 1 } },
+      //     'postcss-loader'
+      //   ]
+      // })
+      use: [
+        'style-loader',
+        { loader: 'css-loader', options: { importLoaders: 1 } },
+        'postcss-loader'
+      ]
+    }
+  ]},
   plugins: [
     new CleanWebpackPlugin(['public']),
-    new HtmlWebpackPlugin({
-      title: 'Starter'
-    }),
-    new ExtractTextPlugin('[name].bundle.css'),
+    new HtmlWebpackPlugin({ title: 'Starter' }),
+    // new ExtractTextPlugin('[name].bundle.css'),
+    new webpack.HotModuleReplacementPlugin(),
     new Dashboard()
   ],
   devtool: '#eval-cheap-module-source-map', //inline-source-map
   devServer: { 
-    contentBase: './public'
+    contentBase: './public',
+    hot: true
   },
   cache: true,
   output: {
